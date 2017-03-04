f9e468c637bc18fb47810a1703d2fff340f20b01
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-03-05 01:20:43 +0200

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,16 @@ require('require-dir')('./gulp');
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
+
+var webserver = require('gulp-webserver');
+ 
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true,
+      host: 0.0.0.0,
+      port: 8000
+    }));
+});
