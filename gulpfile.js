1e7da948b5830b57d59dca831dbc4415a1f9a3f2
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2017-03-04 23:56:04 +0000

    gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
-
-gulp.paths = {
-  src: 'src',
-  dist: 'dist',
-  tmp: '.tmp',
-  e2e: 'e2e'
-};
-
-require('require-dir')('./gulp');
-
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
-
-var webserver = require('gulp-webserver');
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
  
 gulp.task('webserver', function() {
-  gulp.src('app')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true,
-      host: 0.0.0.0,
-      port: 8000
-    }));
+  connect.server({
+    livereload: true,
+    host:'0.0.0.0',
+    port:'8090'
+  });
 });
+ 
+gulp.task('default', ['webserver']);
