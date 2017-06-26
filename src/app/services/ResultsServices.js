b7052fedbe15b99a283e29c4831ad7a8eb6ace6f
ayham <aytaleb91@hotmail.com>
2017-06-27 01:17:09 +0300

    ResultsUrlsServices

---
+++ src/app/services/ResultsServices.js
@@ -0,0 +1,16 @@
+'use strict'
+/* global angular */
+
+var app = angular.module('angularMaterialAdmin');
+var ResultsUrl = "http://127.0.0.1:8000/core_services"
+app.service('listAvailableResultsFiles', function ($resource, BackEnd) {
+  return $resource(ResultsUrl + '/listAvailableResultsFiles', {});
+});
+
+app.service('listAvailableResultsPrefix', function ($resource, BackEnd) {
+  return $resource(ResultsUrl + '/listAvailableResultsPrefix', {});
+});
+
+app.service('listAvailableResultsLog', function ($resource, BackEnd) {
+  return $resource(ResultsUrl + 'logs/listAvailableResultsLog', {});
+});
